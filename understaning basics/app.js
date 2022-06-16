6f1914ffc7958154f2ac447630ec674e9a3cfa9f
ario <cybera.3s@gmail.com>
2022-06-16 19:24:26 +0430

    routing requests

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -13,8 +13,17 @@ const http = require('http');
 // approach 3
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
+
+    const url = req.url;
+    // check if url is '/'
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><input type="submit"></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>App Node JS</title></head>')
