8921ceed09f618a752fe1c81a951b5df651293a5
ario <cybera.3s@gmail.com>
2022-06-16 20:35:32 +0430

    parsing request bodies

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -1,36 +1,44 @@
 const http = require('http');
-
-// approach 1
-// function rqListener(req, res){
-
-// }
-
-// approach 2
-// http.createServer(function(req, res){
-
-// })
-
-// approach 3
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-
+    
     const url = req.url;
-    // check if url is '/'
+    const method = req.method;
+
     if (url === '/') {
-        res.write('<html>')
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><input type="submit"></form></body>')
-        res.write('</html>')
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
         return res.end();
     }
 
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>App Node JS</title></head>')
-    res.write('<body><h1>Hello ario</h1></body>')
-    res.write('</html>')
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const msg = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', msg);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
     res.end();
-
 });
-
+console.log('listening on port 3000...')
 server.listen(3000);
