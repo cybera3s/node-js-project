6899e29a79fb9dcd7d43a8c12a388c004ddc1ea3
ario <cybera.3s@gmail.com>
2022-06-16 19:05:52 +0430

    send responses

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -13,7 +13,15 @@ const http = require('http');
 // approach 3
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    // console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>App Node JS</title></head>')
+    res.write('<body><h1>Hello ario</h1></body>')
+    res.write('</html>')
+    res.end();
+
 });
 
 server.listen(3000);
