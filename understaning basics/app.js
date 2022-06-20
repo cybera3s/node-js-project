ddb6a580c0c750ca0d957e855f5eded3746987a7
ario <cybera.3s@gmail.com>
2022-06-20 16:14:03 +0430

    add expres

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -1,10 +1,12 @@
+// const routes = require('./routes');
+
 const http = require('http');
-const routes = require('./routes');
 
+const express = require('express');
 
-console.log(routes.someText)
+const app = express();
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 console.log('listening on port 3000...')
 server.listen(3000);
