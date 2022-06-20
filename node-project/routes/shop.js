1f5f35051d0ca6c91414c17af22f413c5cebc5fb
ario <cybera.3s@gmail.com>
2022-06-20 19:00:15 +0430

    set path more clear

--- node-project/routes/shop.js
+++ node-project/routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 const express = require('express');
+
+const rootdir = require('../util/path')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
+    res.sendFile(path.join(rootdir, 'views', 'shop.html'));
 });
 
 module.exports = router;
