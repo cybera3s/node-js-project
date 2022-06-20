6d4d8f19ac6092cccb087c60e519700c7a7ed848
ario <cybera.3s@gmail.com>
2022-06-20 16:56:02 +0430

    handling routes

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -5,17 +5,21 @@ const express = require('express');
 const app = express();
 
 
-
-app.use((req, res, next) => {
-    console.log('in the middleware ')
-    next(); // alows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('this always run')
+    next();
 });
 
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in the another middleware ')
+    res.send('<h1>add product page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('in the another middleware ')
     res.send('<h1>hi there</h1>');
 });
-
 
 app.listen(3000, () => {
     console.log('listening on port 3000...')
