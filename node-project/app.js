fa3e91ff8009787713c4bdcd4c7dcb13f19cecf6
ario <cybera.3s@gmail.com>
2022-06-20 17:52:55 +0430

    filtering routes

--- node-project/app.js
+++ node-project/app.js
@@ -16,7 +16,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
--- node-project/routes/shop.js
+++ node-project/routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>hi there</h1>');
+    res.send('<h1>Index Page, hi there</h1>');
 });
 
 module.exports = router;
