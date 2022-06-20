63ebe9e9f3c08a21c63c68d999001093536993bf
ario <cybera.3s@gmail.com>
2022-06-20 17:35:22 +0430

    register routers

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -1,33 +1,27 @@
 // const routes = require('./routes');
 // const http = require('http');
 
+// const server = http.createServer(app);
+// server.listen(3000, ()=>{
+//     console.log('listening on port 3000...')
+// });
+
+
 const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="submit"></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>hi there</h1>');
-});
 
 app.listen(3000, () => {
     console.log('listening on port 3000...')
 
 });
 
-
-// const server = http.createServer(app);
-// server.listen(3000, ()=>{
-//     console.log('listening on port 3000...')
-// });
