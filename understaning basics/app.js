31f73757eeca363abb8bb975ec903502b9dc8908
ario <cybera.3s@gmail.com>
2022-06-20 16:32:28 +0430

    add middlewares

--- understaning basics/app.js
+++ understaning basics/app.js
@@ -6,7 +6,22 @@ const express = require('express');
 
 const app = express();
 
+
+
+app.use((req, res, next) => {
+    console.log('in the middleware ')
+    next(); // alows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('in the another middleware ')
+    
+});
+
 const server = http.createServer(app);
 
-console.log('listening on port 3000...')
-server.listen(3000);
+
+
+server.listen(3000, ()=>{
+    console.log('listening on port 3000...')
+});
