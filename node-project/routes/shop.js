ec1a25a38fe182757827cc3522dbf9ceb12bd1be
ario <cybera.3s@gmail.com>
2022-06-21 15:15:57 +0430

    import products

--- node-project/routes/shop.js
+++ node-project/routes/shop.js
@@ -3,9 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootdir = require('../util/path')
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log("shopjs: ", adminData.products)
     res.sendFile(path.join(rootdir, 'views', 'shop.html'));
 });
 
