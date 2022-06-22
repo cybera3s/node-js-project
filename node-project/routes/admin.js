e5486330873980f09cb6820066dd73222ded6f4d
ario <cybera.3s@gmail.com>
2022-06-22 16:37:52 +0430

    render add-product.pug file

--- node-project/routes/admin.js
+++ node-project/routes/admin.js
@@ -9,10 +9,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: "Add Product"
+    });
 });
 
-// /admin/add-product => POST
+// /admin/add-prod uct => POST
 router.post('/add-product', (req, res, next) => {
     // console.log(req.body);
     products.push({
