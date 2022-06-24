9e06a4251f5abab97875f130dc2ace2d2196a9b0
ario <cybera.3s@gmail.com>
2022-06-24 18:05:42 +0430

    add activeShop context

--- node-project/routes/shop.js
+++ node-project/routes/shop.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
     res.render('shop', {prods: products, pageTitle: 'Shop',
         path: '/',
         hasProducts: products.length > 0,
+        activeShop: true,
 })
 });
 
