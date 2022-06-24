21c9657e037a91d1c54d9f47a34568761ebcf4d5
ario <cybera.3s@gmail.com>
2022-06-24 19:10:51 +0430

    refactor routes

--- node-project/routes/admin.js
+++ node-project/routes/admin.js
@@ -2,32 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require("../controllers/products");
 const router = express.Router();
 
-const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {
-        pageTitle: "Add Product",
-        path: '/admin/add-product',
-        activeProduct: true,
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true,
-    });
-});
+router.get('/add-product', productController.getAddProduct);
 
 // /admin/add-prod uct => POST
-router.post('/add-product', (req, res, next) => {
-    // console.log(req.body);
-    products.push({
-        title: req.body.title
-    });
-    res.redirect('/')
-});
+router.post('/add-product', productController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
