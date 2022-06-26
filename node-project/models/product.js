e180495fd7a747bd3bbc5e6cee0de8c94663c8db
ario <cybera.3s@gmail.com>
2022-06-26 16:41:46 +0430

    refactor save method > Product model save data in file

--- node-project/models/product.js
+++ node-project/models/product.js
@@ -1,3 +1,8 @@
+// imports
+const fs = require('fs');
+const path = require('path');
+
+// declarations
 const products = [];
 
 module.exports = class Product {
@@ -6,7 +11,25 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+                path.dirname(process.mainModule.filename),
+                'data',
+                'products.json'
+            );
+        fs.readFile(p, (err, fileContent) => {
+            console.log(fileContent);
+            console.log(err);
+            let products = [];
+
+            if(!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log("error in writing new product : ", err)
+            });
+            
+        });
     }
 
     static fetchAll() {
