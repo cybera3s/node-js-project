1cff46044cb9d9a05581c03fcc72ce5ae9593be7
ario <cybera.3s@gmail.com>
2022-06-27 23:21:54 +0430

    add getIndex ctrl

---
+++ node-project/controllers/shop.js
@@ -0,0 +1,28 @@
+const Product = require('../models/product');
+
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true
+        });
+    });
+    
+};
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'index',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true
+        });
+    });
+};
+
