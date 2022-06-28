d1792e42e55300be39e5630a88f10029a0a7df62
ario <cybera.3s@gmail.com>
2022-06-28 17:14:12 +0430

    add getEditProduct route

--- node-project/routes/admin.js
+++ node-project/routes/admin.js
@@ -16,6 +16,7 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 
 module.exports = router;
