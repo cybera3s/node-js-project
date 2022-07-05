a3cef98326232ef74b726675418d5714414a7b34
ario <cybera.3s@gmail.com>
2022-07-05 16:59:38 +0430

    refactor product model and delete file references

--- node-project/models/product.js
+++ node-project/models/product.js
@@ -1,27 +1,11 @@
 // imports
-const fs = require('fs');
-const path = require('path');
+
 const Cart = require('./cart');
 
-
 // declarations
-const products = [];
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
-);
 
-const getProductsFromFile = callback => {
-    
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            return callback([]);
-        } else {
-            callback(JSON.parse(fileContent));
-        }
-    });
-};
+
+
 
 module.exports = class Product {
     constructor(id, title, imageUrl, description, price) {
@@ -34,50 +18,20 @@ module.exports = class Product {
 
     save() {
         
-        getProductsFromFile(products => {
-            // updating existing product
-            if (this.id){
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products] ;
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log("error in writing new product : ", err)
-                });
-            } else {
-                this.id = Math.random().toString();
-
-                products.push(this)
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log("error in writing new product : ", err)
-                });
-            }
-            
-        });
+       
     }
 
     static deleteById(id) {
 
-        getProductsFromFile(products =>{
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            const product = products.find(p => p.id = id)
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
-                console.log(err)
-            });
-        });
+       
 
     }
 
     static fetchAll(callback) {
-        getProductsFromFile(callback)
+        
     }
 
     static findById(id, callback) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id == id);
-            callback(product);
-        });
+       
     }
 }
