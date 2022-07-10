bd79953cebcb9d16429fbaad5b4ee4f69445913a
ario <cybera.3s@gmail.com>
2022-07-10 15:32:38 +0430

    refactor id field > cart model

--- node-project/models/cart.js
+++ node-project/models/cart.js
@@ -4,11 +4,11 @@ const sequelize = require('../util/database');
 
 const Cart = sequelize.define('cart', {
     id:{
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
     }
-}),
+})
 
 module.exports = Cart;
