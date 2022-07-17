2504b4e346a858463f3c65b41a02b564e0f48d43
ario <cybera.3s@gmail.com>
2022-07-17 20:52:21 +0430

    add order model

---
+++ node-project/models/order.js
@@ -0,0 +1,13 @@
+const sequelize = require("../util/database");
+const Sequelize = require("sequelize");
+
+const Order = sequelize.define("order", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  }
+});
+
+module.exports = Order;
