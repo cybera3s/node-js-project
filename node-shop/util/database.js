70bd4be59c4cfbc055c6d315163a5641afa08d64
ario <cybera.3s@gmail.com>
2022-08-02 11:40:32 +0430

    refactor db data with env variables

--- node-shop/util/database.js
+++ node-shop/util/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'node_complete',
-    'ario',
-    'S@j_06029842',
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
     {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
     }
 );
 
