c356439ae5f478b332df9d3a1efd4db61497acd6
ario <cybera.3s@gmail.com>
2022-08-08 18:32:24 +0430

    refactor postEditProduct with mongoose

--- node-shop/controllers/admin.js
+++ node-shop/controllers/admin.js
@@ -62,15 +62,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDescription,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageUrl = updatedImageUrl;
+      return product.save()
+  })
     .then((result) => {
       res.redirect("/admin/products");
     })
@@ -89,7 +88,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
