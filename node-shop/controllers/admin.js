d44749715a4b31219bd798c6a78a4a463d73caf8
ario <cybera.3s@gmail.com>
2022-08-08 18:38:09 +0430

    refactor postDeleteProduct with mongoose

--- node-shop/controllers/admin.js
+++ node-shop/controllers/admin.js
@@ -79,9 +79,9 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
-      console.log("destroyed product");
+      console.log("Destroyed product");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
