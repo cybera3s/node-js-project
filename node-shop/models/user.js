8a5648a84395254772f9794760003b35c7007ef8
ario <cybera.3s@gmail.com>
2022-08-11 01:44:30 +0430

    add addToCart method user model

--- node-shop/models/user.js
+++ node-shop/models/user.js
@@ -25,30 +25,30 @@ const userSchema = Schema({
   },
 });
 
-userSchema.methods.addToCart = function(product) {
-    const cartProductIndex = this.cart.items.findIndex((cp) => {
-      return cp.productId.toString() === product._id.toString();
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
     });
+  }
 
-    let newQuantity = 1;
-    const updatedCartItems = [...this.cart.items];
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
 
-    if (cartProductIndex >= 0) {
-      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updatedCartItems[cartProductIndex].quantity = newQuantity;
-    } else {
-      updatedCartItems.push({
-        productId: product._id,
-        quantity: newQuantity,
-      });
-    }
-
-    const updatedCart = {
-      items: updatedCartItems,
-    };
-    this.cart = updatedCart;
-    return this.save();
-  
-}
 
 module.exports = mongoose.model("User", userSchema);
