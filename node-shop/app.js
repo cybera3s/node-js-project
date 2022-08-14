c9a154a4536e15e034f92a77f93051518ea19915
ario <cybera.3s@gmail.com>
2022-08-14 15:53:16 +0430

    add authRoutes

--- node-shop/app.js
+++ node-shop/app.js
@@ -19,6 +19,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // handle 404 page
 app.use(errorController.get404);
