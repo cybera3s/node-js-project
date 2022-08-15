917151b6152ba1a9e81cf60bf6b99dbd36396022
ario <cybera.3s@gmail.com>
2022-08-15 18:47:19 +0430

    get user from session and set to every request

--- node-shop/app.js
+++ node-shop/app.js
@@ -39,6 +39,18 @@ app.use(
   })
 );
 
+// get user from session and add to every request
+app.use((req, res, next)=> {
+  if (!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
