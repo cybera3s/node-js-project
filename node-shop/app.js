07c8aa5c6ead7e9794e547c537f37cb72e27feda
ario <cybera.3s@gmail.com>
2022-08-15 17:51:01 +0430

    store sessions in db

--- node-shop/app.js
+++ node-shop/app.js
@@ -6,12 +6,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require("./controllers/error");
 
 const User = require("./models/user");
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URL,
+  collection: 'sessions',
+});
 
 // template engine config
 
@@ -26,7 +31,12 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({ 
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false, 
+    store: store
+  })
 );
 
 // add user to request
