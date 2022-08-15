5d653ba06952a18bd85191cd1a988cbb61542f3e
ario <cybera.3s@gmail.com>
2022-08-15 18:59:59 +0430

    change logout method to get request

--- node-shop/routes/auth.js
+++ node-shop/routes/auth.js
@@ -8,6 +8,6 @@ router.get("/login", authController.getLogin);
 
 router.post("/login", authController.postLogin);
 
-router.post('/logout', authController.postLogout);
+router.get('/logout', authController.getLogout);
 
 module.exports = router;
