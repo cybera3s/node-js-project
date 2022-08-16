328350e25c8d80093d0473c0f7dadcf8c177e386
ario <cybera.3s@gmail.com>
2022-08-16 17:26:57 +0430

    remove extra isAuthenticated context from controllers

--- node-shop/controllers/admin.js
+++ node-shop/controllers/admin.js
@@ -5,7 +5,6 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -49,7 +48,6 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -101,7 +99,6 @@ exports.getProducts = (req, res, next) => {
         path: "/admin/products",
         hasProducts: products.length > 0,
         activeShop: true,
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
