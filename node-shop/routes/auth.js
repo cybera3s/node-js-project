c5506d43a4b137be28b1911c6645b24cbd2ed3df
ario <cybera.3s@gmail.com>
2022-08-16 14:36:07 +0430

    add signup routes

--- node-shop/routes/auth.js
+++ node-shop/routes/auth.js
@@ -8,6 +8,10 @@ router.get("/login", authController.getLogin);
 
 router.post("/login", authController.postLogin);
 
+router.get("/signup", authController.getSignUp);
+
+router.post("/signup", authController.postSignUp);
+
 router.get('/logout', authController.getLogout);
 
 module.exports = router;
