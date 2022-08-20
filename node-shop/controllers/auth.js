4acf10cca658b3e7f57114f987b51edd9a41a65f
ario <cybera.3s@gmail.com>
2022-08-20 15:10:07 +0430

    add getNewPassword ctrl

--- node-shop/controllers/auth.js
+++ node-shop/controllers/auth.js
@@ -149,11 +149,11 @@ exports.postReset = (req, res, next) => {
       return res.redirect("/reset");
     }
     const token = buffer.toString("hex");
-    
+
     User.findOne({ email: req.body.email })
       .then((user) => {
-        if (!user){
-          req.flash('error', 'No account with that email found');
+        if (!user) {
+          req.flash("error", "No account with that email found");
           return res.redirect("/reset");
         }
 
@@ -173,6 +173,27 @@ exports.postReset = (req, res, next) => {
           `,
         });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+
+      res.render("auth/new-password", {
+        path: "new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString()
+      });
+    })
+    .catch((err) => console.log(err));
 };
