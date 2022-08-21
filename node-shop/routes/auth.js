6cecd7b8f6690144a52343195d168c3f8c6f651f
ario <cybera.3s@gmail.com>
2022-08-21 15:55:40 +0430

    add validation > postSignUp

--- node-shop/routes/auth.js
+++ node-shop/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -10,16 +12,40 @@ router.post("/login", authController.postLogin);
 
 router.get("/signup", authController.getSignUp);
 
-router.post("/signup", authController.postSignUp);
+router.post(
+  "/signup",
+  [
+    check("email", "Please Enter a Valid Email")
+      .isEmail()
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-Mail exists already");
+          }
+        });
+      }),
+    body(
+      "password",
+      "Please Enter a password with at least 4 characters."
+    ).isLength({ min: 4 }),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignUp
+);
 
 router.get("/logout", authController.getLogout);
 
-router.get('/reset', authController.getReset);
+router.get("/reset", authController.getReset);
 
-router.post('/reset', authController.postReset);
+router.post("/reset", authController.postReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.get("/reset/:token", authController.getNewPassword);
 
-router.post('/new-password', authController.postNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
