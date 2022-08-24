4c9897fcf704027d7d3f365733f545be99f067b8
ario <cybera.3s@gmail.com>
2022-08-24 13:30:36 +0430

    add error middleware

--- node-shop/app.js
+++ node-shop/app.js
@@ -79,6 +79,11 @@ app.use(authRoutes);
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
+// error middleware
+app.use((error, req, res, next) => {
+  res.redirect("/500");
+});
+
 const PORT = 3000;
 
 mongoose
