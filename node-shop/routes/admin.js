06848520235f63dbe3967485a17f30ded614c6d8
ario <cybera.3s@gmail.com>
2022-08-24 12:37:52 +0430

    add validation >  postEditProduct

--- node-shop/routes/admin.js
+++ node-shop/routes/admin.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const express = require("express");
+const { body } = require("express-validator");
 
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
@@ -35,7 +36,22 @@ router.get("/products", isAuth, adminController.getProducts);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post("/edit-product", isAuth, 
+[
+    body('title', "Title must only contain letters and numbers with maximum length of 150 characters.")
+    .isAlphanumeric(['en-US'], {'ignore': ' '})
+    .isLength({ min: 2, max: 150 })
+    .trim(),
+    body('imageUrl', "Please enter a valid URL")
+    .isURL(),
+    body('price', "Price must be Integer or Decimal and greater than 0")
+    .isFloat({ gt: 0 }),
+    body('description', "Description must only contain letters and numbers")
+    .isAlphanumeric(['en-US'], {'ignore': ' '})
+    .isLength({ min: 10, max: 400 })
+    .trim()
+], 
+adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
