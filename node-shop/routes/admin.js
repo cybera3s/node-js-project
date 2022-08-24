70878297229165cb85219be727fe175d2845256a
ario <cybera.3s@gmail.com>
2022-08-24 12:37:33 +0430

    add validation > postAddProduct

--- node-shop/routes/admin.js
+++ node-shop/routes/admin.js
@@ -12,7 +12,23 @@ const router = express.Router();
 router.get("/add-product", isAuth, adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post("/add-product",
+ isAuth,
+[
+    body('title', "Title must only contain letters and numbers with maximum length of 150 characters.")
+    .isAlphanumeric(['en-US'], {'ignore': ' '})    
+    .isLength({ min: 2, max: 150 })
+    .trim(),
+    body('imageUrl', "Please enter a valid URL")
+    .isURL(),
+    body('price', "Price must be Integer or Decimal and greater than 0")
+    .isFloat({ gt: 0 }),
+    body('description', "Description must only contain letters and numbers")
+    .isAlphanumeric(['en-US'], {'ignore': ' '})    
+    .isLength({ min: 10, max: 400 })
+    .trim()
+], 
+adminController.postAddProduct);
 
 // /admin/products => GET
 router.get("/products", isAuth, adminController.getProducts);
