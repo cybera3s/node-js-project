b4f8efc070a83947386c37256897e7d8b5434e34
ario <cybera.3s@gmail.com>
2022-08-25 22:58:58 +0430

    refactor getProducts ctrl with pagination

--- node-shop/controllers/shop.js
+++ node-shop/controllers/shop.js
@@ -6,22 +6,39 @@ const pdfDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = process.env.ITEMS_PER_PAGE || 10;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then((products) => {
-      res.render("shop/product-list", {
-        prods: products,
-        pageTitle: "All Products",
-        path: "/products",
+    const page = +req.query.page || 1;
+    let totalItems;
+  
+    Product.find()
+      .countDocuments()
+      .then((numProducts) => {
+        totalItems = numProducts;
+  
+        return Product.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE);
+      })
+      .then((products) => {
+        res.render("shop/product-list", {
+          prods: products,
+          pageTitle: "All Products",
+          path: "/products",
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+        });
+      })
+      .catch((err) => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
       });
-    })
-    .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
 };
 
 exports.getIndex = (req, res, next) => {
