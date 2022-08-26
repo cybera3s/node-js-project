c67d9e4933c1044405638776e5e169870f0776d9
ario <cybera.3s@gmail.com>
2022-08-26 14:11:19 +0430

    refactor deleteProduct ctrl

--- node-shop/controllers/admin.js
+++ node-shop/controllers/admin.js
@@ -160,8 +160,8 @@ exports.postEditProduct = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
 
   Product.findById(prodId)
     .then((product) => {
@@ -174,12 +174,10 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("Destroyed product");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Product Deleted" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting Product Failed." });
     });
 };
 
