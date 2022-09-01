43cb981f6e64c5e72c37de7eb3a1986b7f94886c
ario <cybera.3s@gmail.com>
2022-09-02 01:18:50 +0430

    comment out deleteFile

--- node-shop/util/file.js
+++ node-shop/util/file.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
-
+/**
+ * Delete file with provided path
+ * @param {*} filePath file path
+ */
 const deleteFile = (filePath) => {
     fs.unlink(filePath, (err) => {
         if (err){
