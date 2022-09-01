b1cab1d88942a51afe47c35574f9f71235fc9ff9
ario <cybera.3s@gmail.com>
2022-09-02 00:34:41 +0430

    refactor is-auth middleware

--- node-shop/middleware/is-auth.js
+++ node-shop/middleware/is-auth.js
@@ -1,7 +1,12 @@
-// check if user is logged in
-module.exports = (req, res, next) => {
+/**
+  check if isLoggedIn session has been set 
+  otherwise redirect to login page
+*/
+const isAuth = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     return res.redirect("/login");
   }
   next();
 };
+
+module.exports = isAuth;
